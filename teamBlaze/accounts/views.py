e835a2fbff090091840b80e2c7505d28f4f95856
Zachary Moore <zmoore17@yahoo.com>
2018-10-03 15:11:34 -0400

    update

--- teamBlaze/accounts/views.py
+++ teamBlaze/accounts/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.forms import login, logut
 
 def signup_view(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('Home:Home')
+            return redirect('accounts:login')
     else:
         form = UserCreationForm()
     return render(request, 'accounts/signup.html', {'form' : form })
@@ -16,7 +17,18 @@ def login_view(request):
     if request.method == "POST":
         form = AuthenticationForm(data = request.POST)
         if form.is_valid():
-            return redirect('roleChoice:roleChoice')
+            #log user in
+            user = form.get_user()
+
+            return redirect('article:list')
+
+
     else:
         form = AuthenticationForm()
     return render(request,"accounts/login.html",{'form':form})
+
+
+def logout_view(request):
+    if request.method =='POST'
+        logout(request)
+        return redirect('')
