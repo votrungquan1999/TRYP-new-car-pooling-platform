8429e5985469680d90830ef69b2b5d3cfb9e3e3d
votrungquan1999 <32785143+votrungquan1999@users.noreply.github.com>
2018-10-03 00:54:03 -0400

    update

--- teamBlaze/rolechoice/views.py
+++ teamBlaze/rolechoice/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def promt_user():
+def promt_user(request):
     return HttpResponse("THIS IS A SITE WHERE YOU ASK USER TO CHOOSE THE ROLE")
