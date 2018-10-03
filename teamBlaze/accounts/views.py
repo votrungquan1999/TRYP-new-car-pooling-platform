5b02293b92c982f5a0d848cd2ed5d2cce59d7233
Zachary Moore <zmoore17@yahoo.com>
2018-10-03 15:18:58 -0400

    update

--- teamBlaze/accounts/views.py
+++ teamBlaze/accounts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth.forms import login, logut
+from django.contrib.auth import login, logout
 
 def signup_view(request):
     if request.method == 'POST':
@@ -29,6 +29,6 @@ def login_view(request):
 
 
 def logout_view(request):
-    if request.method =='POST'
+    if request.method =='POST':
         logout(request)
         return redirect('')
