9f77371c6015f6125f0f28148c73d30d591e5a97
votrungquan1999 <32785143+votrungquan1999@users.noreply.github.com>
2018-10-23 11:20:51 -0400

    update

--- teamBlaze/driver_interface/views.py
+++ teamBlaze/driver_interface/views.py
@@ -33,7 +33,7 @@ def post_car_pool(request):
             user = User.objects.get(id=user_id)
             car_pool_post = CarPoolPost(seats=seats, destination_state=destination_state, city=city, price=price,
                                         bags=bags, date=date, time=time, user=user)
-            car_pool_post.save()
+            #car_pool_post.save()
             return redirect('driver_interface:driver_view')
         return render(request, 'driver_interface/create_car_pool.html', {'form':form})
     else:
